ce877fa3a48e6167e216be25a31fbf892aad5ce9
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2025-08-19 09:39:10 -0600

    VISTAS PARA EL DASHBOARD

--- sadi/core/views.py
+++ sadi/core/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from metas.models import Meta  # Ejemplo, importa tus modelos reales
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout as auth_logout, login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+@login_required
+def dashboardView(request):
+    return render(request, "core/dashboard.html")
+
+
+def index(request):
+    return render(request, "index.html")
