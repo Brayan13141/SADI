3919020b47b6fe375674516207baa1d3d6eb374d
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2025-09-19 15:12:58 -0600

    Modelos actualizados

--- sadi/actividades/models.py
+++ sadi/actividades/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from metas.models import Meta
+from departamentos.models import Departamento
 from usuarios.models import Usuario
 from simple_history.models import HistoricalRecords
 
@@ -9,11 +10,46 @@ class Actividad(models.Model):
     descripcion = models.TextField()
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
-    evidencia = models.CharField(max_length=500, blank=True, null=True)
+    editable = models.BooleanField(default=True)
     meta = models.ForeignKey(Meta, on_delete=models.RESTRICT)
     responsable = models.ForeignKey(Usuario, on_delete=models.RESTRICT)
+    departamento = models.ForeignKey(
+        Departamento, on_delete=models.RESTRICT, null=True, blank=True
+    )
 
     history = HistoricalRecords()
 
     def __str__(self):
         return self.descripcion
+
+
+class Evidencia(models.Model):
+    actividad = models.ForeignKey(
+        Actividad, related_name="evidencias", on_delete=models.CASCADE
+    )
+    archivo = models.FileField(upload_to="actividades/evidencias/")
+    fecha_subida = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Bloquear edición si ya existe
+        if self.pk:
+            old = Evidencia.objects.get(pk=self.pk)
+            if old.archivo != self.archivo:
+                raise ValueError("No se puede modificar un archivo existente.")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.archivo.name}"
+
+
+class SolicitudReapertura(models.Model):
+    actividad = models.ForeignKey("Actividad", on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    departamento = models.ForeignKey(
+        "departamentos.Departamento", on_delete=models.SET_NULL, null=True
+    )
+    fecha_solicitud = models.DateTimeField(auto_now_add=True)
+    aprobada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Solicitud de {self.usuario.username} para {self.actividad.descripcion}"
