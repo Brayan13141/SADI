659f0d3c8df2d8c491e4c7addf087e9022c4a4c7
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2025-09-19 15:36:41 -0600

    Vistas actualizadas

--- sadi/usuarios/views.py
+++ sadi/usuarios/views.py
@@ -1,13 +1,15 @@
 from .models import Usuario
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UsuarioEditForm, UsuarioForm
 from django.http import JsonResponse
+from .decorators import role_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 
-@login_required
-def crud_usuarios(request):
+@staff_member_required
+@role_required("ADMIN")
+def gestion_usuarios(request):
     usuarios = Usuario.objects.all()
     form = UsuarioForm()
     abrir_modal_crear = False
@@ -16,13 +18,10 @@ def crud_usuarios(request):
         if "crear_usuario" in request.POST:
             form = UsuarioForm(request.POST)
             if form.is_valid():
-                usuario = form.save(commit=False)
-
+                usuario = form.save()
                 messages.success(request, "Usuario creado correctamente.")
                 return redirect("gestion_usuarios")
-
             else:
-                # Marcar para abrir el modal de creación con errores
                 abrir_modal_crear = True
 
         elif "editar_usuario" in request.POST:
@@ -34,15 +33,11 @@ def crud_usuarios(request):
                 messages.success(request, "Usuario editado correctamente.")
                 return redirect("gestion_usuarios")
             else:
-                # Si hay errores en la edición, devolverlos como JSON para mostrarlos en el modal
                 if request.headers.get("X-Requested-With") == "XMLHttpRequest":
-                    # Convertir errores a formato serializable
                     errors = {}
                     for field, error_list in form.errors.items():
                         errors[field] = [str(error) for error in error_list]
                     return JsonResponse({"errors": errors}, status=400)
-
-                # Si no es AJAX, recargar la página
                 messages.error(request, "Error al editar el usuario.")
 
         elif "eliminar_usuario" in request.POST:
@@ -54,6 +49,6 @@ def crud_usuarios(request):
 
     return render(
         request,
-        "usuarios/crud_usuarios.html",
+        "usuarios/gestion_usuarios.html",
         {"usuarios": usuarios, "form": form, "abrir_modal_crear": abrir_modal_crear},
     )
