806d665da9bfd33b07a6eae68def524fa39ca3ef
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2025-10-10 12:53:24 -0600

    SETTINGS ACTUALIZADOS

--- sadi/sadi/settings.py
+++ sadi/sadi/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     # REST_FRAMEWORK
     "rest_framework",
     "rest_framework.authtoken",
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
     "metas",
     "actividades",
     "riesgos",
+    "reportes",
 ]
 
 MIDDLEWARE = [
