5253f7549eb5efed13e15d6367f6f37e02b8b273
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2025-10-16 11:06:28 -0600

    URLS ACTUALIZADAS

--- sadi/reportes/urls.py
+++ sadi/reportes/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.gestion_reportes, name="reportes"),
     path("metas/", views.reporte_metas_departamento, name="reporte_metas_departamento"),
+    path("programas/", views.reporte_proyectos, name="reporte_programas"),
+    path("avances/", views.reporte_avances_metas, name="reporte_avances_metas"),
+    path("riesgos/", views.reporte_riesgos, name="reporte_riesgos"),
+    path("docentes/", views.reporte_general_docente, name="reporte_docente"),
 ]
