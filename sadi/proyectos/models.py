c5b0801f699fb8f8b9d9ccc0c5af67c90e945a6a
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2025-10-29 09:59:49 -0600

    CORRECCIONES PARA LA GENERACION DE LA CLAVE

--- sadi/proyectos/models.py
+++ sadi/proyectos/models.py
@@ -11,3 +11,19 @@ class Proyecto(models.Model):
 
     def __str__(self):
         return self.clave
+
+    def save(self, *args, **kwargs):
+        # --- GENERAR CLAVE AUTOMÁTICA ---
+        if not self.clave or self.clave.strip().upper() == "AUTO":
+            # Obtenemos la clave del objetivo
+            clave_objetivo = self.objetivo.clave
+            count = Proyecto.objects.filter(objetivo=self.objetivo).count() + 1
+            nueva_clave = f"{clave_objetivo}-PRY{count}"
+            # Evitar duplicados si alguien crea varios a la vez
+            while Proyecto.objects.filter(clave=nueva_clave).exists():
+                count += 1
+                nueva_clave = f"{clave_objetivo}-PRY{count}"
+
+            self.clave = nueva_clave
+
+        super().save(*args, **kwargs)
