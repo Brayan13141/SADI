930838b7501ffd6603b8f1c03c0e17e00b74c224
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2025-10-29 09:38:36 -0600

    CLAVE OBJETIVOS

--- sadi/objetivos/models.py
+++ sadi/objetivos/models.py
@@ -4,6 +4,7 @@ from simple_history.models import HistoricalRecords
 
 
 class ObjetivoEstrategico(models.Model):
+    clave = models.CharField(max_length=50, unique=True, blank=True, null=True)
     descripcion = models.TextField()
     # ciclo = models.ForeignKey(Ciclo, on_delete=models.CASCADE, related_name="objetivos")
     programa = models.ForeignKey(
@@ -13,3 +14,16 @@ class ObjetivoEstrategico(models.Model):
 
     def __str__(self):
         return self.descripcion[:50]
+
+    def save(self, *args, **kwargs):
+        # --- GENERAR CLAVE AUTOMÁTICA ---
+        if not self.clave or self.clave.strip().upper() == "AUTO":
+            # Obtenemos la clave base del programa
+            clave_programa = self.programa.clave
+            count = (
+                ObjetivoEstrategico.objects.filter(programa=self.programa).count() + 1
+            )
+            # Creamos la nueva clave
+            self.clave = f"{clave_programa}-OBJ{count}"
+
+        super().save(*args, **kwargs)
