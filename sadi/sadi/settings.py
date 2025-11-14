6a1c8e5ea7ac9628a6fd1ab031f9bad8f2ac7bf0
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2025-11-14 09:53:44 -0600

    SETTINGS ACTUALIZADO

--- sadi/sadi/settings.py
+++ sadi/sadi/settings.py
@@ -4,6 +4,11 @@ from decouple import config
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
+SITE_NAME = "SADI"
+DOMAIN = "sadi.surguanajuato.tecnm.mx"
+# Dirección base para los enlaces en correos
+DEFAULT_FROM_EMAIL = f"no-reply@{DOMAIN}"
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOG_DIR = BASE_DIR / "logs"
